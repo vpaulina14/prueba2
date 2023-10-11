1c7e5527adb0b0badd0a380affc9474e304a5c5f
unknown <paulinavalenciac123@gmail.com>
2023-10-11 13:51:30 -0500

    prueba branch

--- arreglo3.js
+++ arreglo3.js
@@ -20,4 +20,5 @@ var sumaPares = numerosPares.reduce(function(acumulador, numero) {
 console.log("Números pares:", numerosPares);
 console.log("Suma de números pares:", sumaPares);
 
-//sbhdbfebfybkf rjjfr
+//sbhdbfebfybkf rjjfrjnrfjsndj
+//paulina develop 
