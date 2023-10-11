f11bd924664d12f095848a1d7f9001547c04b5bd
unknown <paulinavalenciac123@gmail.com>
2023-10-11 10:40:27 -0500

    Archivos javascript añadidos

---
+++ arreglo3.js
@@ -0,0 +1,16 @@
+// Crear un array de números
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Filtrar los números pares
+var numerosPares = numeros.filter(function(numero) {
+  return numero % 2 === 0;
+});
+
+// Sumar los números pares
+var sumaPares = numerosPares.reduce(function(acumulador, numero) {
+  return acumulador + numero;
+}, 0);
+
+// Mostrar el resultado en la consola
+console.log("Números pares:", numerosPares);
+console.log("Suma de números pares:", sumaPares);
